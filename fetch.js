10667c6fc6d58213021618a5140869a8c05ad7bf
Verma <harshit.verma@fastmarkets.com>
2026-04-24 05:15:30 +0800

    simplify fetch via cloudflare

--- fetch.js
+++ fetch.js
@@ -1,39 +1,16 @@
 
 const axios = require('axios');
 const xml2js = require('xml2js');
-const { wrapper } = require('axios-cookiejar-support');
-const tough = require('tough-cookie');
 
-const jar = new tough.CookieJar();
-const client = wrapper(axios.create({ jar }));
-
-const NSE_URL = 'https://nse-tracker-harshit351worker.harshit351.workers.dev/';
-
-const HEADERS = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36',
-  'Accept': 'application/xml, text/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'Referer': 'https://www.nseindia.com/',
-  'Connection': 'keep-alive'
-};
+const NSE_URL = 'https://nse-tracker-harshit351worker.harshit351.workers.dev';
 
 async function fetchNSE() {
   try {
-    // Step 1: Hit homepage to get cookies
-    await client.get('https://www.nseindia.com', {
-      headers: HEADERS
-    });
-
-    // Small delay (important)
-    await new Promise(r => setTimeout(r, 1500));
-
-    // Step 2: Fetch XML
-    const res = await client.get(NSE_URL, {
-      headers: HEADERS,
+    const res = await axios.get(NSE_URL, {
       timeout: 15000
     });
 
-    console.log('Fetched XML');
+    console.log('Fetched XML via proxy');
 
     const parsed = await xml2js.parseStringPromise(res.data);
 
@@ -53,4 +30,3 @@ async function fetchNSE() {
 }
 
 module.exports = { fetchNSE };
-
